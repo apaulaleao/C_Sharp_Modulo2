b6f455b46289fb237666f50dd2530dfbbdef1515
Ana Paula Leão <paula.leao@gmail.com>
2022-06-27 11:22:37 -0300

    Adicionar arquivos de projeto.

---
+++ Gerenciamento_salario/Plano_salario.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciando_salario
+{
+    internal class Empregado
+    {
+        public string nome;
+        public string cargo;
+        public double reajusteSalarial;
+
+        public Empregado(string nome, string cargo, double salarioMensal)
+        {
+            this.nome = nome;
+            this.cargo = cargo;
+            this.reajusteSalarial = salarioMensal;
+            // this.reajusteSalarial = reajusteSalarial;
+        }
+        public void SalarioMensal()
+        {
+            double meusalario = this.reajusteSalarial;
+            double reajusteSalarial;
+
+            if (meusalario < 0)
+            {
+                meusalario = 0.0;
+            }
+            else if (meusalario >= 0 && meusalario <= 400.00)
+            {
+                reajusteSalarial = meusalario * 1.15;
+                Console.WriteLine($"Parabéns. Seu salário reajustado é R$: {reajusteSalarial.ToString("F2")}");
+            }
+            else if (meusalario >= 401.00 && meusalario <= 800.00)
+            {
+                reajusteSalarial = meusalario * 1.12;
+                Console.WriteLine($"Parabéns. Seu salário reajustado é R$: {reajusteSalarial.ToString("F2")}");
+            }
+            else if (meusalario >= 800.01 && meusalario <= 12.000)
+            {
+                reajusteSalarial = meusalario * 1.10;
+                Console.WriteLine($"Parabéns.Seu salário reajustado é R$: {reajusteSalarial.ToString("F2")}");
+            }
+            else if (meusalario >= 12.001 && meusalario <= 20.000)
+            {
+                reajusteSalarial = meusalario * 1.07;
+                Console.WriteLine($"Parabéns.Seu salário reajustado é R$: {reajusteSalarial.ToString("F2")}");
+            }
+            else
+            {
+
+                reajusteSalarial = meusalario * 1.04;
+                Console.WriteLine($"Parabéns.Seu salário reajustado é R$: {reajusteSalarial.ToString("F2")}");
+
+            }
+
+        }
+        //return reajusteSalarial;
+
+        public void ReajusteSalario()
+        {
+            Console.WriteLine($"Nome do funcionário: {nome}");
+            Console.WriteLine($"Cargo do funcionário: {cargo}");
+            Console.WriteLine($"Seu novo salário é: {reajusteSalarial.ToString("F2")}");
+        }
+
+    }
+}
---
+++ Gerenciamento_salario/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciando_salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ("** Gerenciamento de salario **");
+            string nome;
+            string cargo;
+            double salario;
+            int iniciar;
+
+
+            Console.WriteLine();
+            Console.Write("Informe nome funcionario: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Informe cargo do funcionario: ");
+            cargo = Console.ReadLine();
+
+            Console.Write("Informe salario do funcionário: ");
+            salario = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Empregado empregado = new Empregado(nome, cargo, salario);
+            do
+            {
+                Console.WriteLine("Escolha as opçoes abaixo: ");
+                Console.WriteLine("(1) Aumento de salário");
+                Console.WriteLine("(2)Exibir meu novo salario");
+                Console.WriteLine("(3)Sair");
+                Console.WriteLine();
+                Console.Write("Escolha sua opção: ");
+                iniciar = int.Parse(Console.ReadLine());
+
+
+                switch (iniciar)
+                {
+                    case 1:
+                        empregado.ReajusteSalario();
+                        break;
+                    case 2:
+                        empregado.SalarioMensal();
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        break;
+                }
+            } while (iniciar != 3);
+        }
+    }
+}
+
